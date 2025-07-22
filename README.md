f6632ea928193574a9cd73e26c2f29f9199962f2
Pramod Anandarao <panandarao@wisc.edu>
2025-07-22 16:47:15 -0500

    changed config to ingest cities as JSON

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 Create a `.env` file at the project root with these values:
 ```
 OPENAI_API_KEY=[YOUR OPENAI KEY]
-CITIES=[COMMA SEPERATED LIST OF CITIES]
 API_KEYS=[COMMA SEPERATED LIST OF API KEYS]
+CITIES={"Test" : "http://localhost:8081"}
 ```
 You can optionally add the `REDIS_URL` and `POLL_INTERVAL` if necessary otherwise they will default to `redis://redis:6379/0` and `60`, respectively.
 
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,13 @@
 from functools import lru_cache
-from pydantic import Field
+from pydantic import Field, AnyUrl
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     openai_api_key: str = Field(..., env='OPENAI_API_KEY')
+    api_keys: str = Field(..., env='API_KEYS')
     redis_url: str = Field('redis://redis:6379/0', env='REDIS_URL')
     poll_interval: int = Field(60, env='POLL_INTERVAL')
-    cities: str = Field(..., env='CITIES')
-    api_keys: str = Field(..., env='API_KEYS')
+    cities: dict[str, AnyUrl] = Field(..., env='CITIES')
 
     class Config:
         env_file = '.env'
