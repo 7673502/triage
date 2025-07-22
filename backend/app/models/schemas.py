5faa2cf131fb411ac0edb6134f62b02e9d46daf6
Pramod Anandarao <panandarao@wisc.edu>
2025-07-22 14:47:08 -0500

    removed typing import

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,10 +1,9 @@
-from typing import List
 from pydantic import BaseModel, conint
 from .enums import RequestFlag
 
 class ClassifiedPayload(BaseModel):
     priority: conint(ge=0, le=100)
-    flag: List[RequestFlag]
+    flag: list[RequestFlag]
     priority_explanation: str
     flag_explanation: str
 
