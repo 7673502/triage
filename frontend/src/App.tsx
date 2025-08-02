390e88ecf07c95911baf0a80c4126b187b847553
Pramod Anandarao <panandarao@wisc.edu>
2025-08-02 16:39:03 -0500

    moved card to components and refactored imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ComplaintCard from './ComplaintCard';
+import ComplaintCard from './components/ComplaintCard';
 
 function App() {
   const [count, setCount] = useState(0)
