7a2793d2c5a92bdb1747bf5962e3b1c1a429a035
Pramod Anandarao <panandarao@wisc.edu>
2025-08-03 11:03:00 -0500

    added navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import Complaints from './pages/Complaints';
 import MapPage from './pages/Map';
@@ -8,11 +9,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/complaints" element={<Complaints />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/insights" element={<Insights />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/complaints" element={<Complaints />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/insights" element={<Insights />} />
+        </Route>   
       </Routes>
     </BrowserRouter>
   );
