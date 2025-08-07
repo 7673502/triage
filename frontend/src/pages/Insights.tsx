8500d9bd0848a32c40dee201f17fe8bc1035d02a
Tanvi Shukla <tanvishukla@Tanvis-Laptop.local>
2025-08-07 05:39:35 -0500

    added heatmap to insights page and added labels

--- frontend/src/pages/Insights.tsx
+++ frontend/src/pages/Insights.tsx
@@ -5,8 +5,12 @@ import {
   LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid,
   BarChart, Bar, ResponsiveContainer
 } from 'recharts';
+
 //import Heatmap from "./HeatMap"; // make in future
 import SiteFooter from '../components/SiteFooter';
+
+
+import Heatmap from "./HeatMap"; // make in future
 
 type Point = { lat: number; lng: number };
 export default function Insights() {
@@ -41,7 +45,7 @@ export default function Insights() {
     const buckets = [0, 0, 0, 0, 0];
     items.forEach((req) => {
       const p = req.priority ?? 0;
-      if (p <= 1) buckets[0]++;
+      if (p <= 20) buckets[0]++;
       else if (p <= 40) buckets[1]++;
       else if (p <= 60) buckets[2]++;
       else if (p <= 80) buckets[3]++;
@@ -69,13 +73,13 @@ export default function Insights() {
     
 
     // Heatmap points
-    // setGeoPoints(
-    //   items
-    //     .filter((r): r is { lat: number; long: number } =>
-    //       typeof r.lat === 'number' && typeof r.long === 'number'
-    //     )
-    //     .map((r) => ({ lat: r.lat, lng: r.long }))
-    //);
+    setGeoPoints(
+      items
+        .filter((r): r is { lat: number; long: number } =>
+          typeof r.lat === 'number' && typeof r.long === 'number'
+        )
+        .map((r) => ({ lat: r.lat, lng: r.long }))
+    );
   }, [items]);
 
   if (loading) return <p style={{ textAlign: 'center', paddingTop: 80 }}>Loading…</p>;
@@ -86,27 +90,42 @@ export default function Insights() {
   return (
     <>
     <div style={{maxWidth: 1000, margin: '0 auto' }}>
+
       <h2 style={{ textAlign: 'center' }}>Insights for {cityKey}</h2>
+
+      <h2 style={{ textAlign: 'center' }}></h2>
+ 
 
       <section style={{ marginTop: 30 }}>
         <h2>Requests per Hour (Last 24h)</h2>
         <ResponsiveContainer width="100%" height={300}>
-          <LineChart data={hourlyCounts}>
-            <XAxis dataKey="hour" />
-            <YAxis />
+          <LineChart data={hourlyCounts} margin={{ left: 30, right: 20, top: 20, bottom: 20 }}>
+            <XAxis dataKey="hour" 
+              label={{ value: 'Hour of Day', position: 'outsideBottom', dy: 25 }}
+            />
+            <YAxis 
+              label={{ value: 'Number of Requests', angle: -90, position: 'center', dx: -10}}
+            />
             <Tooltip />
             <CartesianGrid stroke="#ccc" />
             <Line type="monotone" dataKey="count" stroke="#007bff" />
           </LineChart>
         </ResponsiveContainer>
+        {/* <p style={{ textAlign: 'center', marginTop: -8, fontSize: 14, color: '#666' }}>
+          Hour of Day
+        </p> */}
       </section>
 
       <section style={{ marginTop: 48 }}>
         <h2>Request Priority Distribution</h2>
         <ResponsiveContainer width="100%" height={300}>
-          <BarChart data={priorityBuckets}>
-            <XAxis dataKey="bucket" />
-            <YAxis />
+          <BarChart data={priorityBuckets} margin={{ left: 30, right: 20, top: 20, bottom: 20 }}>
+            <XAxis dataKey="bucket" 
+              label={{ value: "Priority Value", dy: 25}}
+            />
+            <YAxis 
+              label = {{value: "Count", angle: -90, dx: -10}}
+            />
             <Tooltip />
             <CartesianGrid stroke="#ccc" />
             <Bar dataKey="count" fill="#28a745" />
@@ -117,11 +136,13 @@ export default function Insights() {
       <section style={{ marginTop: 48 }}>
         <h2>Requests by Service Type</h2>
         <ResponsiveContainer width="100%" height={300}>
-          <BarChart data={serviceCounts}>
+          <BarChart data={serviceCounts} margin={{ left: 30, right: 20, top: 20, bottom: 20 }}>
             <XAxis dataKey="name" interval={0} angle={-45} textAnchor="end" height={120} tickFormatter={(name) =>
     name.length > 20 ? name.slice(0, 18) + '…' : name
   } />
-            <YAxis />
+            <YAxis 
+              label = {{value: "Count", angle: -90, dx: -10}}
+            />
             <Tooltip />
             <CartesianGrid stroke="#ccc" />
             <Bar dataKey="count" fill="#ffc107" />
@@ -129,10 +150,10 @@ export default function Insights() {
         </ResponsiveContainer>
       </section>
 
-      {/* <section style={{ marginTop: 48 }}>
+      <section style={{ marginTop: 48 }}>
         <h2>Complaint Heatmap</h2>
         <Heatmap points={geoPoints} />
-      </section> */}
+      </section>
     </div>
     <SiteFooter />
     </>
